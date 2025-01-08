2dc2d0a509c78a1670351c35f236387997b47da5
daniel <dangeresque8@gmail.com>
2025-01-08 12:09:10 -0800

    first commit

---
+++ src/textnode.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class TextType(Enum):
+    normal_text =  "normal"
+    bold_text = "bold"
+    italic_text = "italic"
+    code_text = "code"
+    link = "link"
+    image = "image"
+
+
+
